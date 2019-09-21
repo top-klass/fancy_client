e64110be31e1990906af5665321a120c3934a313
songpang <scc6920@naver.com>
2019-09-21 21:46:44 +0900

    nav setting

---
+++ Components/MainView.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Platform } from 'react-native';
+
+import StoreTab from './AppTabNavigator/StoreTab';
+import UserTab from './AppTabNavigator/UserTab';
+import WalletTab from './AppTabNavigator/WalletTab';
+
+import { createAppContainer } from 'react-navigation'; 
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+
+const AppTabNavigator = createMaterialTopTabNavigator({
+  WalletTab: { screen: WalletTab },
+  StoreTab: { screen: StoreTab },
+  UserTab: { screen: UserTab }
+}, {
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: true,
+    showLabel: true,
+    showIcon: true,
+  }
+});
+
+const AppTabContainer = createAppContainer(AppTabNavigator);
+
+export default class MainView extends Component {
+  render() {
+    return <AppTabContainer/>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
